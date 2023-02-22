42ceaa34c4a6e89049612131223009a08aa51d73
Thrivikram N <thrivikram.naganna@sophos.com>
2023-02-22 18:51:47 +0530

    fixed.

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-cd /home/ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install --lts
 nvm install 16
-
-cd simplilearn/nodejs
 npm init --y
 npm install --save mysql
 npm install --save express
 npm install --save ejs
-sudo yum -y install mariadb
-
 npm install forever -g
 npm install forever-monitor
 forever start app.js
