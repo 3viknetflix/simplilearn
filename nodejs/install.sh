4883af8eef5cef0b903da575d778de1e6a7d6700
Thrivikram N <thrivikram.naganna@sophos.com>
2023-02-22 17:53:53 +0530

    sudo not required

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -2,15 +2,15 @@
 cd /home/ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
-sudo nvm install --lts
-sudo nvm install 16
+nvm install --lts
+nvm install 16
 
-sudo npm init --y
-sudo npm install --save mysql
-sudo npm install --save express
-sudo npm install --save ejs
+npm init --y
+npm install --save mysql
+npm install --save express
+npm install --save ejs
 sudo yum -y install mariadb
 
-sudo npm install forever -g
-sudo npm install forever-monitor
+npm install forever -g
+npm install forever-monitor
 forever start app.js
